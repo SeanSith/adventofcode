d0267b81347f410f045c2f638f4ce275247edf80
Sean Smith <SeanSith@users.noreply.github.com>
2022-12-05 23:41:12 -0500

    Day 3 adjustment to allow for inspection of all rucksacks, regardless of how many members are in the party

--- day3/part2.rb
+++ day3/part2.rb
@@ -7,8 +7,8 @@ total_priority = 0
 
 File.open('input') do |f|
   f.lazy.each_slice(party_size) do |party_members|
-    rucksacks = party_members.each(&:chomp!).map.to_a
-    party_badge = (rucksacks[0].chars & rucksacks[1].chars & rucksacks[2].chars).first
+    rucksacks = party_members.map { |x| x.chomp!.chars }
+    party_badge = rucksacks.reduce { |memo, items| (memo || items) & items }.first
     total_priority += Rucksack.new('blah').duplicate_priority(party_badge)
   end
 end

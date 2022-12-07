5fdd9e113df39a5bfb7381fc5cff122a33de5627
Sean Smith <SeanSith@users.noreply.github.com>
2022-12-06 21:57:52 -0500

    Day 6

---
+++ day6/part2.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+marker_length = 14
+
+input = File.read('input').chars
+
+marker = []
+
+input.each_with_index do |char, index|
+  marker << char
+  next unless marker.length == marker_length
+
+  if marker.uniq.length == marker.length
+    puts "We processed #{index + 1} characters and found #{marker.join}."
+    exit
+  else
+    marker.shift
+  end
+end
